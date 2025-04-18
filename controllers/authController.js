20ab479711773c34385e428f2e17ef8e8a535098
Ketsebaot Ertumo <ertumoketsebaot@gmail.com>
2025-04-18 09:38:39 +0300

    update roll back

--- controllers/authController.js
+++ controllers/authController.js
@@ -184,10 +184,10 @@ const sendTokenResponse = async (user, statusCode, res) => {
         const token = await user.getJwtToken();
         const cookieOptions = {
             httpOnly: true,
-            secure: true, // Only in HTTPS
-            sameSite: 'None', 
-            // secure: false, // only for local dev!
-            // sameSite: 'Lax', // or 'None' if secure is true
+            // secure: true, // Only in HTTPS
+            // sameSite: 'None', 
+            secure: false, // only for local dev!
+            sameSite: 'Lax', // or 'None' if secure is true
             maxAge: 8 * 60 * 60 * 1000, // 8 hours
         };
         return res.status(statusCode)
