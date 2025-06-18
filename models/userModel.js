3bd8c14929a1550b8843bdbe2843db9aa87a627c
ketsebaot-ertumo <ertumoketsebaot@gmail.com>
2025-06-18 07:48:15 +0300

    update phone null

--- models/userModel.js
+++ models/userModel.js
@@ -35,6 +35,7 @@ module.exports = (sequelize, DataTypes) => {
         phoneNumber: {
             type: DataTypes.STRING,
             allowNull: true,
+            defaultValue: "+251987654321",
             validate: {
               is: /^\+?[1-9]\d{1,14}$/ // E.164 format validation
             }
