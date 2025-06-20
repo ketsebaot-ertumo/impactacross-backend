9b794bc86f9141b52780fa532187b0a22518518c
ketsebaot-ertumo <ertumoketsebaot@gmail.com>
2025-06-20 11:20:23 +0300

    gallery update

--- seeders/index.js
+++ seeders/index.js
@@ -15,32 +15,32 @@ const {
   Team,
   Project,
   Partner,
+  Gallery
 } = require("../models/index");
-// const db = require("../models/index");
-// console.log(db); // 👈 This will show you what is actually exported
 
 
 (async () => {
   try {
-    console.log("Syncing database...");
-    await sequelize.sync({ force: true });
+    // console.log("Syncing database...");
+    // await sequelize.sync({ force: true });
 
-    console.log("Seeding data...");
-    const user = await require("./userSeeder")();
-    await require("./blogSeeder")(user.id);
-    await require("./publicationSeeder")(user.id);
-    await require("./multimediaSeeder")(user.id);
-    await require("./trainingSeeder")(user.id);
-    // await seedBlogs(user.id);
+    // console.log("Seeding data...");
+    // const user = await require("./userSeeder")();
+    // await require("./blogSeeder")(user.id);
+    // await require("./publicationSeeder")(user.id);
+    // await require("./multimediaSeeder")(user.id);
+    // await require("./trainingSeeder")(user.id);
     
-    await require("./ownerSeeder")();
-    await require("./aboutUsSeeder")();
-    const sectionMap = await require("./sectionSeeder")();
-    await require("./contentSeeder")(sectionMap);
-    await require("./serviceSeeder")(sectionMap);
-    await require("./teamSeeder")(sectionMap);
-    await require("./projectSeeder")(sectionMap);
-    await require("./partnerSeeder")(sectionMap);
+    // await require("./ownerSeeder")();
+    // await require("./aboutUsSeeder")();
+    // const sectionMap = await require("./sectionSeeder")();
+    // await require("./contentSeeder")(sectionMap);
+    // await require("./serviceSeeder")(sectionMap);
+    // await require("./teamSeeder")(sectionMap);
+    // await require("./projectSeeder")(sectionMap);
+    // await require("./partnerSeeder")(sectionMap);
+
+    await Gallery.sync({ force: true });
     await require("./gallerySeeders")();
 
     console.log("✅ Seeding complete.");
